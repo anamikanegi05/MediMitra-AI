943e5adc3ce7d56f65d9b77c7e52d68f93594daa
Anamika Negi <157869777+anamikanegi05@users.noreply.github.com>
2026-04-08 19:01:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template, request
 from modules.mcp_server import MCPServer
+from database import get_chats, init_db
 
 app = Flask(__name__)
 mcp = MCPServer()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    response = None
+    suggestion = None
 
     if request.method == "POST":
-        user_input = request.form.get("symptoms")
-        response = mcp.process(user_input)
+        symptoms = request.form.get("symptoms")
+        suggestion = mcp.process(symptoms)
 
-    return render_template("index.html", response=response)
+    chats = get_chats()
 
+    return render_template("index.html", suggestion=suggestion, chats=chats)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    init_db()
+    app.run(debug=True)
